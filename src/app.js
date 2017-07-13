67c69b0d9552667c3a3595b8a37c2553504e383a
Anushka Makhija <amakhija@pivotal.io>
2017-07-12 17:29:32 -0700

    Playing with flexbox
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {DefaultButton} from 'pui-react-buttons';
 import {Icon} from 'pui-react-iconography';
-require("./test.css");
+require("./test.scss");
 
 class App extends React.Component {
   render() {
     return (
       <div className="test">
-        foo
-        bar
-        <DefaultButton>Default</DefaultButton>
-        <Icon src="add"/>
+        <div className="foo child">foo</div>
+        <div className="bar child">bar</div>
+        <DefaultButton className="default child">Default</DefaultButton>
+        <Icon className="child icon" src="add"/>
       </div>
     );
   }
