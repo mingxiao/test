cd8eff1b518feb95d490f5af467a6b4f1c6db341
Ming Xiao <mxiao@pivotal.io>
2017-09-27 14:42:34 -0700

    WIP: autoscale iframe
    Signed-off-by: Stephane Jolicoeur <sjolicoeur@pivotal.io>

--- src/app.js
+++ src/app.js
@@ -6,14 +6,28 @@ import Header from './components/header';
 require("./test.scss");
 
 class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener("message", this.handleFrameTasks);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("message", this.handleFrameTasks);
+  }
+  handleFrameTasks(e) {
+    console.log('localhost:3000', e);
+  };
+
   render() {
     return (
       <div className="test">
         <div className="foo child">foo</div>
         <div className="bar child">bar</div>
-        <DefaultButton className="default child">Default</DefaultButton>
+        <DefaultButton className="default child" onClick={() => alert('foo')}>Default</DefaultButton>
         <Icon className="child icon" src="add"/>
         <Header />
+        <a onClick={() => window.parent.postMessage({msg: "POLO",  provider: 'scaling'}, "*")} > Send Polo</a>
+        <a onClick={() => window.parent.postMessage(this.handleFrameTasks, "*")} > Send func</a>
+        <br/>
+        <iframe src="https://enable-cors.org/"/>
       </div>
     );
   }
